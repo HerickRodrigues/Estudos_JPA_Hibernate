6b9d40affebaa7d23367ca35f931cf506e954f6a
Herick Rodrigues <herickcj@hotmail.com>
2022-08-11 20:32:47 -0300

    commit

---
+++ loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -0,0 +1,27 @@
+package br.com.alura.loja.dao;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.modelo.Produto;
+
+public class ProdutoDao {
+
+	private EntityManager em;
+
+	public ProdutoDao(EntityManager em) {
+		this.em = em;
+	}
+	
+	public void cadastrar(Produto produto) {
+		this.em.persist(produto);
+	}
+	
+	public void atualizar(Produto produto) {
+		this.em.merge(produto);
+	}
+	
+	public void remover(Produto produto) {
+		this.em.remove(produto);
+	}
+	
+}
